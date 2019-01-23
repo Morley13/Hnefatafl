3658eba85eb7d903469ca2546e1c9b782c1212dc
James Morley <907267@swansea.ac.uk>
2019-01-23 15:08:17 +0000

    I have added a method to the move class which calculates all of the pieces possible moves from its column and length.

--- Hnefatafl/src/Move.java
+++ Hnefatafl/src/Move.java
@@ -1,13 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.util.ArrayList;
 
 /**
  *
  * @author James
  */
 public class Move {
+
+    private Board board;
+    private int newX;
+    private int newY;
+
+    public Move(int newX, int newY) {
+        this.newX = newX;
+        this.newY = newY;
+    }
+
+    public int getNewX() {
+        return newX;
+    }
+
+    public int getNewY() {
+        return newY;
+    }
+
+    public ArrayList<Move> getPiecesPossibleMoves(int column, int length) {
+
+        ArrayList<Move> possibleMoves = new ArrayList<>();
+
+        Piece p = this.board.getPieceAtIndex(column, length);
+
+        Move m;
+
+        //get all possible up moves      
+        if (column != 0) {
+            int temp = column;
+            while (board.getPieceAtIndex(temp, length) != null) {
+                m = new Move(temp, length);
+                possibleMoves.add(m);
+                temp++;
+            }
+        }
+
+        //get all possible down moves
+        if (column != board.getBoardLength() - 1) {
+            int temp = length;
+            while (board.getPieceAtIndex(column, temp) != null) {
+                m = new Move(column, temp);
+                possibleMoves.add(m);
+                temp--;
+            }
+        }
+
+        //get all possible left moves
+        if (column != 0) {
+            int temp = column;
+            while (board.getPieceAtIndex(temp, length) != null) {
+                m = new Move(temp, length);
+                possibleMoves.add(m);
+                temp--;
+            }
+        }
+
+        //get all possible right moves
+        if (column != board.getBoardWidth() - 1) {
+            int temp = column;
+            while (board.getPieceAtIndex(temp, length) != null) {
+                m = new Move(temp, length);
+                possibleMoves.add(m);
+                temp++;
+            }
+        }
+        
+        System.out.println(possibleMoves.toString());
+        return possibleMoves;     
+    }
     
+    @Override
+    public String toString() {
+        return "(" + newX + " , " + newY + ")";
+    }
 }
