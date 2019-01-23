eebea923b0d5326038dc3fbed112a5bf524fecee
James Morley <907267@swansea.ac.uk>
2019-01-23 13:53:25 +0000

    I have updated the piece class so it no longer contains any data about the pieces location, this is because the board class is currently handling a pieces location; as a result, I had a class between which class should handle the position of any given piece. At the moment the board class controls the location.

--- Hnefatafl/src/Piece.java
+++ Hnefatafl/src/Piece.java
@@ -4,5 +4,31 @@
  * @author James
  */
 public class Piece {
+        
+    public Piece() {
+
+    }
+    
+//    public void setXPos(int xPos) {
+//        this.xPos = xPos;
+//    }
+//    
+//    public int getXPos() {
+//        return xPos;
+//    }
+//    
+//    public void setYPos(int yPos) {
+//        this.yPos = yPos;
+//    }
+//    
+//    public int getYPos() {
+//        return yPos;
+//    }
+    
+    @Override
+    public String toString() {
+        return "x";
+    }
+    
     
 }
