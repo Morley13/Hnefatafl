0305c9acddf818d4d88829d25dcecab5bce6a743
James Morley <907267@swansea.ac.uk>
2019-01-23 15:08:57 +0000

    I have removed any logic that controls a pieces location, all of the location logic is now in the board class.

--- Hnefatafl/src/Piece.java
+++ Hnefatafl/src/Piece.java
@@ -9,26 +9,8 @@ public class Piece {
 
     }
     
-//    public void setXPos(int xPos) {
-//        this.xPos = xPos;
-//    }
-//    
-//    public int getXPos() {
-//        return xPos;
-//    }
-//    
-//    public void setYPos(int yPos) {
-//        this.yPos = yPos;
-//    }
-//    
-//    public int getYPos() {
-//        return yPos;
-//    }
-    
     @Override
     public String toString() {
         return "x";
-    }
-    
-    
+    }    
 }
